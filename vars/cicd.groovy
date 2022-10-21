caed7d72d45f0751a7b3886ed5539274d5f3fb03
atif10iqbal <113500925+atif10iqbal@users.noreply.github.com>
2022-10-21 10:19:23 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,11 +2,12 @@ def newGit(repo)
 {
   git "${repo}"
 }
-/*
+
 def newMaven()
 {
   sh "mvn package"
 }
+/*
 def newDeploy(job,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war
