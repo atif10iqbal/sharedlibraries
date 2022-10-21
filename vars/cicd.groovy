012a498c4101248925f1cb989a708de6056e7772
atif10iqbal <113500925+atif10iqbal@users.noreply.github.com>
2022-10-21 09:38:15 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,5 +10,9 @@ def newDeploy(job,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war
 }
+def git(repo)
+{
+  git "${repo}"
+}
 
 
