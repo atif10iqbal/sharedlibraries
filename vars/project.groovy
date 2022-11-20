2bd34875223d1b329e66b1c33d2892d84493b820
atif10iqbal <113500925+atif10iqbal@users.noreply.github.com>
2022-11-20 19:30:57 +0530

    Update project.groovy
    h

--- vars/project.groovy
+++ vars/project.groovy
@@ -12,3 +12,7 @@ def newDeployment(job,ip,context)
 {
   sh "scp /root/.jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+ def runSelenium(job)
+{
+  sh "java -jar /root/.jenkins/workspace/${job}/testing.jar"
+}
